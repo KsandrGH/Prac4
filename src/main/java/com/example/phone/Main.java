7e5916c13b91b804f91fe73bd386380451a7093d
Alexander Novikov <snovikov339@gmail.com>
2026-02-16 18:35:13 +0300

    upd

--- src/main/java/com/example/phone/Main.java
+++ src/main/java/com/example/phone/Main.java
@@ -16,6 +16,7 @@ import java.nio.file.Paths;
  */
 public class Main {
 
+
     public static void main(String[] args) {
         if (args.length < 1) {
             System.err.println("Usage: java -jar phone-variant6-1.0.0.jar <inputFile> [outputFile] [--cc=1]");
